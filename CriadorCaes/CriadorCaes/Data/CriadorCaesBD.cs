f0cbc03000c8430d7bb09f342d04c87706072bb5
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-03-26 22:44:25 +0000

    Criação da classe que representa a BD

---
+++ CriadorCaes/CriadorCaes/Data/CriadorCaesBD.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CriadorCaes.Models;
+
+namespace CriadorCaes.Data {
+
+   /// <summary>
+   /// representa a DB dos criadores de caes
+   /// </summary>
+   public class CriadorCaesBD : DbContext {
+
+      // e onde está a BD armazenada?
+
+
+      //************************************************************************
+      // especificar as tabelas na BD
+      //************************************************************************
+      public DbSet<Criadores> Criadores { get; set; }
+      public DbSet<Caes> Caes { get; set; }
+      public DbSet<Fotografias> Fotografias { get; set; }
+      public DbSet<Racas> Racas { get; set; }
+      public DbSet<CriadoresCaes> CriadoresCaes { get; set; }
+      //************************************************************************
+
+   }
+}
